af91e18c0ae6cbfa4ae7cdeedf93e3cffa594357
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-09 17:25:02 +0100

    Add custom quote card component

---
+++ web/src/lib/components/QuoteCard.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let content: string;
+    export let author: string;
+</script>
+
+<div class="card card-hover flex flex-col justify-between p-4">
+    <blockquote class="blockquote">{content}</blockquote>
+    <div class="h-4"></div>
+    <strong>{author}</strong>
+</div>
