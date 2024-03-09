d3f3d6a9f951ecd5dd19535589697f0264b1f898
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-09 16:15:35 +0100

    Finished top of the landing page

---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<div class="flex justify-center items-center h-full gap-8 p-16">
+    <div class="flex flex-col gap-8 w-1/2">
+        <div class="ml-8"><img class="w-24" src="favicon.png" alt="Library caster's icon" /></div>
+        <strong class="text-6xl">Library Caster</strong>
+        <span class="text-2xl">Manage your library like never before! Never forget your best memories with books thnaks to this innovative library managment service.</span>
+        <div class="flex gap-8 m-4">
+            <button type="button" class="btn variant-filled-primary">
+                <span>Check more</span>
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                    </svg>
+                </span>
+            </button>
+            <button type="button" class="btn variant-filled">
+                <span>Get started</span>
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                    </svg>              
+                </span>
+            </button>
+        </div>
+    </div>
+    <img class="w-1/2 rounded-md" src="placeholder.jpeg" alt="Placeholder">
+</div>
