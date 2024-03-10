99e5cdfa1bc9b162c9534265d0dc46ae8581c9be
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-10 17:13:44 +0100

    Include extracted avatar popup component

--- web/src/lib/components/AppBar.svelte
+++ web/src/lib/components/AppBar.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
-	import { AppBar } from '@skeletonlabs/skeleton';
-</script>
+	import { AppBar, Avatar } from '@skeletonlabs/skeleton';
+    import { popup } from '@skeletonlabs/skeleton';
+    import type { PopupSettings } from '@skeletonlabs/skeleton';
+	import AvatarPopup from './AvatarPopup.svelte';
+    
+    const popupFeatured: PopupSettings = {
+        // Represents the type of event that opens/closed the popup
+        event: 'click',
+        // Matches the data-popup value on your popup element
+        target: 'popupFeatured',
+        // Defines which side of your trigger the popup will appear
+        placement: 'bottom',
+    };
+					
+</script>			
 
+<AvatarPopup dataPopup="popupFeatured"/>
 
 <AppBar>
     <svelte:fragment slot="lead">
@@ -15,29 +29,14 @@
         </div>
     </svelte:fragment>
     <svelte:fragment slot="trail">
-        <a
-            class="btn btn-sm variant-ghost-surface"
-            href="https://discord.gg/EXqV7W8MtY"
-            target="_blank"
-            rel="noreferrer"
-        > 
-            Discord
-        </a>
-        <a
-            class="btn btn-sm variant-ghost-surface"
-            href="https://twitter.com/SkeletonUI"
-            target="_blank"
-            rel="noreferrer"
-        >
-            Twitter
-        </a>
-        <a
-            class="btn btn-sm variant-ghost-surface"
-            href="https://github.com/skeletonlabs/skeleton"
-            target="_blank"
-            rel="noreferrer"
-        >
-            GitHub
-        </a>
+        <a href="/" class="btn bg-initial font-semibold">Home</a>
+        <a href="/pricing" class="btn bg-initial font-semibold">Pricing</a>
+        <a href="/dashboard" class="btn bg-initial font-semibold">Dashboard</a>
+        <button type="button" class="btn bg-initial" use:popup={popupFeatured}>
+            <Avatar src="placeholder.jpeg" width="w-10" rounded="rounded-full" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+            </svg>              
+        </button>
     </svelte:fragment>
 </AppBar>
